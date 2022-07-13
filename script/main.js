92c02452bf42f69ff0fa2e8479d406d1422451cd
MrKrasnov <mr.krasnov.y@gmail.com>
2022-07-13 15:23:04 +0300

    2.8 created anchor's links

--- script/main.js
+++ script/main.js
@@ -1,6 +1,24 @@
+// logic for actively style cards title
 let cards_with_answer = document.querySelectorAll('.answers_cards_card');
 
 for (let item of cards_with_answer) {
     item.addEventListener("mouseover", e => e.target.classList.add('answers_cards_active'));
     item.addEventListener("mouseout", e => e.target.classList.remove('answers_cards_active'));
+}
+
+// anchors links
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
 }
